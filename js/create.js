42d246f50f501a2dc432e6df61c77818e0e11d35
JioChoi <jiochoi7@gmail.com>
2023-04-04 11:37:47 +0900

    Updated Files

--- js/create.js
+++ js/create.js
@@ -1,6 +1,6 @@
-function createItem() {
+function createItem(parent) {
 	let element = document.createElement("img");
-	element.src = "data/image/test.png";
+	element.src = "/parent/" + parent + ".png";
 	element.classList.add("item");
 	element.ondragstart = function() { return false; };
 	document.getElementById("mainContainer").appendChild(element);
