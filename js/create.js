7a68ec13af43ffa98a885d57884a8939c49d091e
JioChoi <jiochoi7@gmail.com>
2023-04-05 14:29:21 +0900

    No Image image

--- js/create.js
+++ js/create.js
@@ -1,6 +1,9 @@
 function createItem(parent) {
 	let element = document.createElement("img");
-	element.src = "/parent/" + parent + ".png";
+	element.src = "/group/" + parent + ".png";
+	element.onerror = function error() {
+		this.src = "data/images/assets/NoImage.png";
+	}
 	element.classList.add("item");
 	element.ondragstart = function() { return false; };
 	document.getElementById("mainContainer").appendChild(element);
